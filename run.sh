4d5277d7b60731be4588559a4f6a2ee9ee0c197a
Alexander Kleinschmidt <kleinschmidt@mobile2b.de>
2018-11-27 17:28:47 +0100

    fixes

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t kungalex/rest-mqtt-proxy .
-gnome-terminal -x sh -c "docker run -it \
-    -e APP_CONFIG_FILE=/srv/rest-mqtt-proxy/config.ini \
-    -e PRE_MAPPING_DIR=/srv/rest-mqtt-proxy/mappings \
-    -v ${PWD}/config/prod/config.ini:/srv/rest-mqtt-proxy/config.ini:ro \
-    -v ${PWD}/config/prod/mappings/:/srv/rest-mqtt-proxy/mappings/:ro \
-    kungalex/rest-mqtt-proxy:latest"
